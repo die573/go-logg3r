9d246406178e2399aab50c14f92a43beea0c70a7
die573 <diegocard573@gmail.com>
2025-07-28 01:51:02 +0200

    add main.go add Makefile

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	go build -o go-logg3r main.go
+run:
+	go run main.go
+clean:
+	rm -f go-logg3r
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("A simple Go keylogger implementation for educational purposes.")
+}
